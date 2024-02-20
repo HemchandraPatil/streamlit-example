448ccb61c5cec46de04be0374baf11f1c9a6e57e
HemchandraPatil <88281942+HemchandraPatil@users.noreply.github.com>
2024-02-20 14:41:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ import streamlit as st
 st.title("HELLO world!!!!")
 
 st.subheader("This is subheader")
+
+Stage_name =st.selectbox("Stage Name: ", 
+                          ["STAGE1","STAGE2","STAGE3"])
+
+st.write("You have selected ",Stage_name)
