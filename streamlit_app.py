fe37ec1e959b6d2d9fefdc997c3db17dda797861
HemchandraPatil <88281942+HemchandraPatil@users.noreply.github.com>
2024-03-06 17:24:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
+# Import python packages
+import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import sum, col
 import altair as alt
-import streamlit as st
 
 # Set page config
 st.set_page_config(layout="wide")
